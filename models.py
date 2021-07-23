6f46147b8e42e3e0d52a1256a4d49dde3078579d
Li <alex.li03@sap.com>
2021-07-23 15:31:21 +0800

    Initial commit

---
+++ models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime,func
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
+
+class MyUser(Base):
+    __tablename__ = "myuser"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    password =  Column(String)
+    email = Column(String, unique=True, index=True)
+    created_on = Column(DateTime, server_default=func.current_timestamp())
+    modified_on = Column(DateTime, default=func.current_timestamp(),
+                         onupdate=func.current_timestamp())
+    is_active = Column(Boolean, default=True)
+    role = Column(Integer, default=1)
---
+++ schemas.py
@@ -0,0 +1,57 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+import datetime
+
+class ItemBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
+
+
+
+class MyUserBase(BaseModel):
+    email: str
+    username: str
+    is_active: bool
+    role: int
+
+
+class MyUserCreate(MyUserBase):
+    password: str
+
+
+class MyUser(MyUserBase):
+    id: int
+    created_on: datetime.datetime
+    modified_on: datetime.datetime
+    class Config:
+        orm_mode = True
